e08076e9a1d87ca1894589d311ff78064f72f6a5
beguschs <stefan.begusch@gmail.com>
2017-07-17 16:58:35 +0200

    createWorkGroups V2 programm

---
+++ createWorkGroupsV2/createWorkGroupsV2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace createWorkGroupsV2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWGStatement = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPreview = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.rbPrimaryUser = new System.Windows.Forms.RadioButton();
+            this.rbSelectUser = new System.Windows.Forms.RadioButton();
+            this.txtUserStatement = new System.Windows.Forms.TextBox();
+            this.dgvUser = new System.Windows.Forms.DataGridView();
+            this.btnLoadUser = new System.Windows.Forms.Button();
+            this.btnCreateWorkGroup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtWGStatement
+            // 
+            this.txtWGStatement.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWGStatement.Location = new System.Drawing.Point(1, 22);
+            this.txtWGStatement.Multiline = true;
+            this.txtWGStatement.Name = "txtWGStatement";
+            this.txtWGStatement.Size = new System.Drawing.Size(564, 84);
+            this.txtWGStatement.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WorkGroup Statement:";
+            // 
+            // dgvPreview
+            // 
+            this.dgvPreview.AllowUserToAddRows = false;
+            this.dgvPreview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPreview.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvPreview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPreview.Location = new System.Drawing.Point(1, 141);
+            this.dgvPreview.Name = "dgvPreview";
+            this.dgvPreview.RowHeadersVisible = false;
+            this.dgvPreview.Size = new System.Drawing.Size(564, 287);
+            this.dgvPreview.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(1, 112);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(564, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Vorschau laden";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // rbPrimaryUser
+            // 
+            this.rbPrimaryUser.AutoSize = true;
+            this.rbPrimaryUser.Checked = true;
+            this.rbPrimaryUser.Location = new System.Drawing.Point(15, 434);
+            this.rbPrimaryUser.Name = "rbPrimaryUser";
+            this.rbPrimaryUser.Size = new System.Drawing.Size(202, 17);
+            this.rbPrimaryUser.TabIndex = 4;
+            this.rbPrimaryUser.TabStop = true;
+            this.rbPrimaryUser.Text = "User über primäre Organisation finden";
+            this.rbPrimaryUser.UseVisualStyleBackColor = true;
+            // 
+            // rbSelectUser
+            // 
+            this.rbSelectUser.AutoSize = true;
+            this.rbSelectUser.Location = new System.Drawing.Point(15, 457);
+            this.rbSelectUser.Name = "rbSelectUser";
+            this.rbSelectUser.Size = new System.Drawing.Size(187, 17);
+            this.rbSelectUser.TabIndex = 5;
+            this.rbSelectUser.Text = "User über Select Statement finden";
+            this.rbSelectUser.UseVisualStyleBackColor = true;
+            this.rbSelectUser.CheckedChanged += new System.EventHandler(this.rbSelectUser_CheckedChanged);
+            // 
+            // txtUserStatement
+            // 
+            this.txtUserStatement.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserStatement.Location = new System.Drawing.Point(223, 431);
+            this.txtUserStatement.Multiline = true;
+            this.txtUserStatement.Name = "txtUserStatement";
+            this.txtUserStatement.Size = new System.Drawing.Size(342, 43);
+            this.txtUserStatement.TabIndex = 6;
+            // 
+            // dgvUser
+            // 
+            this.dgvUser.AllowUserToAddRows = false;
+            this.dgvUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUser.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUser.Location = new System.Drawing.Point(1, 508);
+            this.dgvUser.Name = "dgvUser";
+            this.dgvUser.RowHeadersVisible = false;
+            this.dgvUser.Size = new System.Drawing.Size(564, 90);
+            this.dgvUser.TabIndex = 7;
+            // 
+            // btnLoadUser
+            // 
+            this.btnLoadUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadUser.Location = new System.Drawing.Point(1, 480);
+            this.btnLoadUser.Name = "btnLoadUser";
+            this.btnLoadUser.Size = new System.Drawing.Size(564, 23);
+            this.btnLoadUser.TabIndex = 8;
+            this.btnLoadUser.Text = "Benuter laden";
+            this.btnLoadUser.UseVisualStyleBackColor = true;
+            this.btnLoadUser.Click += new System.EventHandler(this.btnLoadUser_Click);
+            // 
+            // btnCreateWorkGroup
+            // 
+            this.btnCreateWorkGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreateWorkGroup.Location = new System.Drawing.Point(1, 604);
+            this.btnCreateWorkGroup.Name = "btnCreateWorkGroup";
+            this.btnCreateWorkGroup.Size = new System.Drawing.Size(564, 23);
+            this.btnCreateWorkGroup.TabIndex = 9;
+            this.btnCreateWorkGroup.Text = "Arbeitsgruppen anlegen und Benutzer zuordnen";
+            this.btnCreateWorkGroup.UseVisualStyleBackColor = true;
+            this.btnCreateWorkGroup.Click += new System.EventHandler(this.btnCreateWorkGroup_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 633);
+            this.Controls.Add(this.btnCreateWorkGroup);
+            this.Controls.Add(this.btnLoadUser);
+            this.Controls.Add(this.dgvUser);
+            this.Controls.Add(this.txtUserStatement);
+            this.Controls.Add(this.rbSelectUser);
+            this.Controls.Add(this.rbPrimaryUser);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvPreview);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWGStatement);
+            this.Name = "frmMain";
+            this.Text = "createWorkGroups";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWGStatement;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPreview;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.RadioButton rbPrimaryUser;
+        private System.Windows.Forms.RadioButton rbSelectUser;
+        private System.Windows.Forms.TextBox txtUserStatement;
+        private System.Windows.Forms.DataGridView dgvUser;
+        private System.Windows.Forms.Button btnLoadUser;
+        private System.Windows.Forms.Button btnCreateWorkGroup;
+    }
+}
+
---
+++ createWorkGroupsV2/createWorkGroupsV2/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WFLOBJ;
+using oraClient = Oracle.ManagedDataAccess.Client;
+using oraType = Oracle.ManagedDataAccess.Types;
+
+namespace createWorkGroupsV2
+{
+    public partial class frmMain : Form
+    {
+        const string USER_CODE = "BIG";
+
+        public SCBWflSession Session { get; set; }
+        public string ConnectionString { get; set; }
+        public oraClient.OracleConnection oracleConnection { get; set; }
+        public frmMain()
+        {
+            InitializeComponent();
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("select 49 OENR, 'OM Team Wien 1' OEBEZ, 'OFM W1' OE_KURZBEZ, ");
+                sb.AppendLine("'OFM W1' || substr(username, instr(username, '_')) ARBEITSGRUPPE ");
+                sb.AppendLine("from mitarbeiter where username like 'W1%' and ist_rolle = 1 and status = 0 ");
+                sb.AppendLine("order by 1");
+                txtWGStatement.Text = sb.ToString();
+                
+                txtUserStatement.Text = "select USERNR, USERNAME from mitarbeiter where usernr = 11";
+
+                CCDGUI.ccdLogin login = new CCDGUI.ccdLogin();
+                if (login.DoLogin())
+                {
+                    Session = login.Session;
+                    SCBWflRows rows = Session.SQLCommand(USER_CODE, "select value from BIG_CONNECTION_STRING");
+                    foreach (SCBWflRow row in rows)
+                    {
+                        ConnectionString = row.ToString();
+                        oracleConnection = new oraClient.OracleConnection(ConnectionString);
+                        oracleConnection.Open();
+                    }
+                }
+                else
+                {
+                    this.Close();
+                }
+                rbSelectUser.Checked = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in frmMain()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvPreview.Columns.Clear();
+
+                oraClient.OracleCommand cmd = oracleConnection.CreateCommand();
+                cmd.CommandText = txtWGStatement.Text;
+                using (oraClient.OracleDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        dgvPreview.DataSource = dataTable;
+                    }
+                    reader.Close();
+                    reader.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in btnLoad_Click()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                oracleConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in frmMain_FormClosing()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void rbSelectUser_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbSelectUser.Checked)
+            {
+                txtUserStatement.Enabled = true;
+                dgvUser.Enabled = true;
+                btnLoadUser.Enabled = true;
+            }
+            else
+            {
+                txtUserStatement.Enabled = false;
+                btnLoadUser.Enabled = false;
+                dgvUser.Columns.Clear();
+                dgvUser.Enabled = false;
+            }
+        }
+
+        private void btnLoadUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvUser.Columns.Clear();
+
+                oraClient.OracleCommand cmd = oracleConnection.CreateCommand();
+                cmd.CommandText = txtUserStatement.Text;
+                using (oraClient.OracleDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        dgvUser.DataSource = dataTable;
+                    }
+                    reader.Close();
+                    reader.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in btnLoadUser_Click()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCreateWorkGroup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rbSelectUser.Checked && dgvUser.Rows.Count > 0)
+                { //user über select statement
+                    createWorkGroup();
+                }
+
+                if (rbPrimaryUser.Checked)
+                { // user über die primary OE finden
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in btnCreateWorkGroup_Click()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void createWorkGroup()
+        {
+            try
+            {
+                int OENR = 0;
+                int userID = 0;
+                SCBWflOrganization oe;
+                SCBWflUser user;
+                SCBWflWorkGroup wg;
+                bool checkFlag = false;
+                foreach (DataGridViewRow row in dgvPreview.Rows)
+                {
+                    try
+                    {
+                        checkFlag = true;
+                        OENR = Convert.ToInt32(row.Cells["OENR"].Value.ToString());
+                        oe = Session.System.GetOrganizationByID(Session.System.NewIDByLocalKey(OENR));
+                        wg = Session.System.NewWorkGroup();
+                        wg.SetOrganization(oe);
+                        wg.Name = row.Cells["ARBEITSGRUPPE"].Value.ToString();
+
+                        wg.Create();
+                    }
+                    catch (Exception ex)
+                    {
+                        checkFlag = false;
+                        MessageBox.Show(ex.Message, "Error in SCBWflWorkGroup.Create()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        try
+                        {
+                            wg = Session.System.FindWorkGroups("Name like '" + row.Cells["ARBEITSGRUPPE"].Value.ToString() + "'").Item(1);
+                            checkFlag = true;
+                        }
+                        catch
+                        {
+                            checkFlag = false;
+                        }
+                    }
+                    try
+                    {
+                        if (checkFlag)
+                        {
+                            foreach (DataGridViewRow userRow in dgvUser.Rows)
+                            {
+                                userID = Convert.ToInt32(userRow.Cells["USERNR"].Value.ToString());
+                                user = Session.System.GetUserByID(Session.System.NewIDByLocalKey(userID));
+                                user.AssignWorkGroup(wg);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error in SCBWflUser.AssignWorkGroup()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in createWorkGroup()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
