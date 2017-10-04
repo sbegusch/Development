c4ab3dacf1b5c7f23047bf15b9a3f25ac7ddacc4
beguschs <stefan.begusch@gmail.com>
2017-10-04 15:38:47 +0200

    fertig

--- myAdminTool/CHECK.sql
+++ myAdminTool/CHECK.sql
@@ -1,21 +1,29 @@
 /***************** PRODUKTION **************/
-select * from V_BIG_WORKGROUP_MAPPING where source_name like 'W%' and source_name = destination_name order by source_name;  --7 Zeilen
+select * from V_BIG_WORKGROUP_MAPPING order by oebez_neu, destination_name;
 
-select * from BIG_WORKGROUP_MAPPING 
-where source_id in (1219,154,1109,152,1411,153,155,  --> WU Arbeitsgruppen
-  1224,1005,1114,1007,1416,1008,1006,2104);         --> W1 Arbeitsgruppen
+--select * from BIG_WORKGROUP_MAPPING where DESTINATION_NAME like '%0010%';
+--update BIG_WORKGROUP_MAPPING set DESTINATION_NAME = REPLACE(DESTINATION_NAME, '-0010', '') where DESTINATION_NAME like '%0010%';
+--commit;
 
---> UPDATE
---update BIG_WORKGROUP_MAPPING set SOURCE_NAME = 'w' || SOURCE_NAME 
---where source_id in (1219,154,1109,152,1411,153,155,  --> WU Arbeitsgruppen
---    1224,1005,1114,1007,1416,1008,1006,2104);         --> W1 Arbeitsgruppen
+--> MAPPING TABLE
+select * from BIG_WORKGROUP_MAPPING where (source_name like 'W1%' or source_name like 'W2%'); --> WIRD ZU "W1"
+--update BIG_WORKGROUP_MAPPING set SOURCE_NAME = '-' || SOURCE_NAME where (source_name like 'W1%' or source_name like 'W2%'); 
+
+select * from BIG_WORKGROUP_MAPPING where (source_name like 'W3%' or source_name like 'W4%'); --> WIRD ZU "W2"
+--update BIG_WORKGROUP_MAPPING set SOURCE_NAME = '-' || SOURCE_NAME where (source_name like 'W3%' or source_name like 'W4%'); 
+
+select * from BIG_WORKGROUP_MAPPING where (source_name like 'W5%' or source_name like 'W6%'); --> WIRD ZU "W3"
+--update BIG_WORKGROUP_MAPPING set SOURCE_NAME = '-' || SOURCE_NAME where (source_name like 'W5%' or source_name like 'W6%'); 
+
 commit;
 
-select * from mitarbeiter 
-where usernr in (1219,154,1109,152,1411,153,155,  --> WU Arbeitsgruppen
-1224,1005,1114,1007,1416,1008,1006,2104);         --> W1 Arbeitsgruppen
---> UPDATE
---update mitarbeiter set username = 'w' || username 
---where usernr in (1219,154,1109,152,1411,153,155,  --> WU Arbeitsgruppen
---1224,1005,1114,1007,1416,1008,1006,2104);         --> W1 Arbeitsgruppen
+--> MITARBEITER
+select * from MITARBEITER where (username like 'W1%' or username like 'W2%') and status = 0 and ist_rolle = 1; --> WIRD ZU "W1"
+--update MITARBEITER set username = '-' || username where (username like 'W1%' or username like 'W2%') and status = 0 and ist_rolle = 1; 
+select * from MITARBEITER where (username like 'W3%' or username like 'W4%') and status = 0 and ist_rolle = 1; --> WIRD ZU "W2"
+--update MITARBEITER set username = '-' || username where (username like 'W3%' or username like 'W4%') and status = 0 and ist_rolle = 1; 
+select * from MITARBEITER where (username like 'W5%' or username like 'W6%') and status = 0 and ist_rolle = 1; --> WIRD ZU "W3"
+--update MITARBEITER set username = '-' || username where (username like 'W5%' or username like 'W6%') and status = 0 and ist_rolle = 1; 
+
 commit;
+
